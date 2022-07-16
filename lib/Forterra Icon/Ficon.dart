6b305eee6a304bfcededcf7c1e09fcc1b5271238
Rishi Vora <87199706+rishijv@users.noreply.github.com>
2022-07-16 00:14:12 -0700

    addition of menu to home page

---
+++ lib/Forterra Icon/Ficon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Forterra {
+  Forterra._();
+  static const _kFontFam = 'Forterra';
+  static const IconData logo = IconData(
+    0xe800,
+    fontFamily: _kFontFam,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:cut_my_carbon/Forterra%20Icon/Ficon.dart';
 import 'package:flutter/material.dart';
-
-import 'ui/home_view.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,32 +22,42 @@ class _MyAppState extends State<MyApp> {
         //scaffoldBackgroundColor: const Color.fromARGB(255, 49, 48, 48),
         //primarySwatch: Colors.green,
         home: Scaffold(
+            floatingActionButton: SpeedDial(
+              animatedIcon: AnimatedIcons.menu_close,
+              backgroundColor: Colors.green,
+              spacing: 15,
+              children: [
+                SpeedDialChild(
+                    child: Icon(Icons.settings_rounded),
+                    backgroundColor: Colors.green,
+                    onTap: () {}),
+                SpeedDialChild(
+                    child: Icon(Icons.mail_rounded),
+                    backgroundColor: Colors.green,
+                    onTap: () {}),
+                SpeedDialChild(
+                    child: Icon(Icons.feedback_rounded),
+                    backgroundColor: Colors.green,
+                    onTap: () {}),
+                SpeedDialChild(
+                    child: Icon(Icons.tips_and_updates_rounded),
+                    backgroundColor: Colors.green,
+                    onTap: () {}),
+                SpeedDialChild(
+                    child: Icon(Icons.info_rounded),
+                    backgroundColor: Colors.green,
+                    onTap: () {}),
+                SpeedDialChild(
+                    child: Icon(Forterra.logo),
+                    backgroundColor: Colors.green,
+                    onTap: () {}),
+              ],
+            ),
             backgroundColor: Color.fromARGB(255, 119, 188, 63),
             body: Center(
                 child: Column(children: [
-              Container(
-                padding: EdgeInsets.fromLTRB(0, 40, 10, 0),
-                alignment: Alignment.topRight,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Icon(Icons.email, size: 20),
-                  style: ElevatedButton.styleFrom(
-                      shape: CircleBorder(), minimumSize: Size.square(40)),
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                  padding: EdgeInsets.fromLTRB(0, 10, 10, 0),
-                  alignment: Alignment.topRight,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Icon(Icons.settings, size: 20),
-                    style: ElevatedButton.styleFrom(
-                        shape: CircleBorder(), minimumSize: Size.square(40)),
-                  )),
-              SizedBox(height: 10),
-              Container(
-                  padding: EdgeInsets.fromLTRB(0, 10, 10, 0),
+              /*Container(
+                  padding: EdgeInsets.fromLTRB(0, 50, 10, 0),
                   alignment: Alignment.topRight,
                   child: ElevatedButton(
                     onPressed: () {},
@@ -56,17 +66,18 @@ class _MyAppState extends State<MyApp> {
                       style: TextStyle(
                         fontFamily: 'Forterra',
                         fontSize: 20,
-                        //fontWeight: FontWeight.bold,
                       ),
                     ), //Icon(Icons.letter_, size: 20),
                     style: ElevatedButton.styleFrom(
                         shape: CircleBorder(), minimumSize: Size.square(40)),
-                  )),
+                  )),*/
+              SizedBox(height: 80),
               Container(
                 child: Image.asset(
                   'assets/Logo.png',
                 ),
               ),
+              SizedBox(height: 60),
               Container(
                   child: ElevatedButton(
                 onPressed: () {
@@ -75,11 +86,20 @@ class _MyAppState extends State<MyApp> {
                 onLongPress: () {
                   print("Tips Long Pressed");
                 },
-                child: Text('Tips'),
+                child: Text(
+                  'Select Your Tip',
+                  style: TextStyle(
+                    fontSize: 40,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(340, 80),
+                  padding: EdgeInsets.all(30),
+                ),
               )),
-              SizedBox(height: 20),
+              SizedBox(height: 30),
               Container(
-                  child: TextButton(
+                  child: ElevatedButton(
                 onPressed: () {
                   print("Statistics Pressed");
                 },
@@ -90,12 +110,11 @@ class _MyAppState extends State<MyApp> {
                   'Statistics',
                   style: TextStyle(
                     fontSize: 40,
-                    backgroundColor: Colors.brown,
                   ),
                 ),
                 style: ElevatedButton.styleFrom(
-                  minimumSize: Size(100, 100),
-                  padding: EdgeInsets.all(10),
+                  minimumSize: Size(340, 80),
+                  padding: EdgeInsets.all(30),
                 ),
               ))
             ]))));
