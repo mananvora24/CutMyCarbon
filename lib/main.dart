857ba5ce75d89a6e833b1aa5939249cde5952d20
jayvee47 <jineshuvora47@gmail.com>
2022-07-16 09:25:34 -0700

    Flutter firebase iOS integration using https://firebase.google.com/docs/flutter/setup?platform=ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:cut_my_carbon/Forterra%20Icon/Ficon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
