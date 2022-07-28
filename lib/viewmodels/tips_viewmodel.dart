7593d932aa340d40aa08e2b998c9b91cf3cb0196
jayvee47 <jineshuvora47@gmail.com>
2022-07-28 00:04:11 -0700

    Added query to pull userTips data and then pass into Tips to fetch only necessary records for tip selection

--- lib/viewmodels/tips_viewmodel.dart
+++ lib/viewmodels/tips_viewmodel.dart
@@ -6,13 +6,36 @@ class TipsViewModel extends SharedViewModel {
   int myTipOrder = 1;
   CollectionReference userTips =
       FirebaseFirestore.instance.collection('UsertTips');
-  String dataText = "";
+  String userTip = "";
+
+  Future<int> getUserCategoryTipOrder(String category, String user) async {
+    await FirebaseFirestore.instance
+        .collection('UserTips')
+        .where('User', isEqualTo: user)
+        .where('Category', isEqualTo: category)
+        .get()
+        .then((QuerySnapshot<Map<String, dynamic>> querySnapshot) {
+      List<dynamic> data = querySnapshot.docs;
+      if (data.isEmpty) {
+        print("Data is empty");
+      }
+      for (var snapshot in data) {
+        Map<String, dynamic>? tipData = snapshot.data();
+        tipData?.forEach((key, value) {
+          myTipOrder = tipData['TipOrder'];
+        });
+      }
+    });
+    return myTipOrder;
+  }
 
   Future<String> getTipForUser(String category, String user) async {
     bool tipFound = false;
+    int tipOrder = await getUserCategoryTipOrder(category, user);
     await FirebaseFirestore.instance
         .collection('Tips')
         .where('Category', isEqualTo: category)
+        .where('TipOrder', isGreaterThan: tipOrder)
         .orderBy('TipOrder')
         .get()
         .then((QuerySnapshot<Map<String, dynamic>> querySnapshot) {
@@ -23,15 +46,16 @@ class TipsViewModel extends SharedViewModel {
       for (var snapshot in data) {
         Map<String, dynamic>? tipData = snapshot.data();
         tipData?.forEach((key, value) {
-          if (tipData['TipOrder'] > myTipOrder && !tipFound) {
+          if (tipData['TipOrder'] > tipOrder && !tipFound) {
             // Found the tip needed
             tipFound = true;
-            dataText = "${tipData['TipOrder']} => ${tipData['Tip']}";
+            userTip = "${tipData['TipOrder']} => ${tipData['Tip']}";
           }
         });
+        break;
       }
     });
-    return dataText;
+    return userTip;
   }
 
   int getMyTipOrder() {
