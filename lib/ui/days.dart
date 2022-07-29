1d5c228032251964fe110264b0aac2c8595d04c1
Rishi Vora <87199706+rishijv@users.noreply.github.com>
2022-07-29 19:17:09 -0400

    days input addition

---
+++ lib/ui/days.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:cut_my_carbon/Forterra Icon/Ficon.dart';
+import 'package:cut_my_carbon/viewmodels/home_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return ChangeNotifierProvider(
+      create: (context) => HomeViewModel(),
+      child: Consumer<HomeViewModel>(
+        builder: (context, model, child) => Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios_new_rounded),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            backgroundColor: const Color.fromARGB(255, 119, 188, 63),
+            elevation: 0,
+          ),
+          backgroundColor: const Color.fromARGB(255, 119, 188, 63),
+          body: Center(
+            child: Column(children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(0, 50, 10, 0),
+                alignment: Alignment.topRight,
+              ),
+              Text('Enter the number of days you completed the activity'),
+              SizedBox(
+                height: 50,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter the days',
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
