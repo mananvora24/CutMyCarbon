7d226791c3bdace40bf7f59fd0a8c8a812dcfd0c
jayvee47 <jineshuvora47@gmail.com>
2022-08-21 23:47:36 -0700

    UI and message - status update view

--- lib/ui/tip_status_update_view.dart
+++ lib/ui/tip_status_update_view.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cut_my_carbon/core/utilities/constants.dart';
+import 'package:cut_my_carbon/viewmodels/tip.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cut_my_carbon/viewmodels/home_viewmodel.dart';
@@ -25,6 +26,13 @@ class TipStatusUpdateView extends StatelessWidget {
     TextEditingController daysController = TextEditingController();
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+    TipsData tipsData = TipsData(
+        category: "",
+        user: "",
+        tipOrder: 0,
+        tip: "",
+        description: "",
+        carbon: 0);
 
     return ChangeNotifierProvider(
       create: (context) => HomeViewModel(),
@@ -58,9 +66,98 @@ class TipStatusUpdateView extends StatelessWidget {
                   padding: const EdgeInsets.fromLTRB(0, 50, 10, 0),
                   alignment: Alignment.topRight,
                 ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+                  padding: const EdgeInsets.all(9.0),
+                  decoration: BoxDecoration(
+                    color: whiteColor,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade400,
+                          spreadRadius: 1,
+                          blurRadius: 15),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: width * 0.8,
+                        child: const Text(
+                          "Tip",
+                          style: TextStyle(
+                              fontFamily: primaryFont,
+                              color: primaryColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: boxTitleFontSize),
+                        ),
+                      ),
+                      const Divider(
+                        color: primaryColor,
+                      ),
+                      SizedBox(
+                        width: width * 0.8,
+                        child: FutureBuilder<TipsData>(
+                            future: model.getCurrentTip(
+                                category, tipOrder, currentUserUsername),
+                            builder: (
+                              BuildContext context,
+                              AsyncSnapshot<TipsData> snapshot,
+                            ) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return const CircularProgressIndicator();
+                              } else if (snapshot.connectionState ==
+                                  ConnectionState.done) {
+                                if (snapshot.hasError) {
+                                  return const Text(
+                                    'getTipForUser returned Error',
+                                    style: TextStyle(
+                                      fontFamily: primaryFont,
+                                      color: primaryColor,
+                                    ),
+                                  );
+                                } else if (snapshot.hasData) {
+                                  tipsData = snapshot.data!;
+                                  //tipOrder = tipsData.tipOrder;
+                                  print("TipsData: $tipsData");
+                                  return Text(tipsData.tip,
+                                      textAlign: TextAlign.left,
+                                      style: const TextStyle(
+                                        fontFamily: primaryFont,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: boxTextFontSize,
+                                        color: primaryColor,
+                                      ));
+                                } else {
+                                  return const Text(
+                                    'Empty data',
+                                    style: TextStyle(
+                                      fontFamily: primaryFont,
+                                      color: primaryColor,
+                                    ),
+                                  );
+                                }
+                              } else {
+                                return Text(
+                                  'State: ${snapshot.connectionState}',
+                                  style: const TextStyle(
+                                    fontFamily: primaryFont,
+                                    color: primaryColor,
+                                  ),
+                                );
+                              }
+                            }),
+                      ),
+                    ],
+                  ),
+                ),
                 SizedBox(
-                  height: 120,
-                  width: width * 0.9,
+                  height: height * 0.02,
+                ),
+                SizedBox(
+                  height: height * 0.08,
+                  width: width * 0.8,
                   child: Text(
                     message,
                     textAlign: TextAlign.center,
@@ -72,7 +169,7 @@ class TipStatusUpdateView extends StatelessWidget {
                   ),
                 ),
                 SizedBox(
-                  width: width * 0.8,
+                  width: width * 0.9,
                   child: const Text(
                       'How many days did you successfully complete your tip?',
                       textAlign: TextAlign.center,
